252530f90919ccbb5349c63d19b45fcfe892d8dc
Shafeeque OT <shafeequeot@gmail.com>
2023-03-07 16:07:04 +0400

    some more rel completed

--- components/global/whatsappIcon.jsx
+++ components/global/whatsappIcon.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa'
 
 function WhatsAppIcon() {
   return (
-   <Link target={"_blank"}  href="https://wa.me/971600553235"><div className='cursor-pointer left-8 bottom-8 fixed w-16 h-16 bg-white rounded-full shadow-lg flex justify-center items-center'><FaWhatsapp size={50} color='green'/></div></Link>
+   <Link target={"_blank"} rel="noopener noreferrer"  href="https://wa.me/971600553235"><div className='cursor-pointer left-8 bottom-8 fixed w-16 h-16 bg-white rounded-full shadow-lg flex justify-center items-center'><FaWhatsapp size={50} color='green'/></div></Link>
   )
 }
 
